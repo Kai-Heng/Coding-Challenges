721b28083bd8f2be6510bc3ba285bb9f5173a899
Kai-Heng <gankaiheng@gmail.com>
2025-06-26 12:57:10 -0500

    Solved a challenge

---
+++ longest_binary_subsequence/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+#include <numeric>
+#include <algorithm>
+#include <math.h>
+#include <unordered_set>
+
+using namespace std;
+
+int longestSubsequence(string s, int k) {
+    int sm = 0;
+    int cnt = 0;
+    int bits = 32 - __builtin_clz(k);
+    for (int i = 0; i < s.size(); ++i) {
+        char ch = s[s.size() - 1 - i];
+        if (ch == '1') {
+            if (i < bits && sm + (1 << i) <= k) {
+                sm += 1 << i;
+                cnt++;
+            }
+        } else {
+            cnt++;
+        }
+    }
+    return cnt;
+}
